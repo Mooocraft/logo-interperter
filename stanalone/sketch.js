d9cb00042ef6bab326e53eba9480684351deb736
Mooocraft <38020594+Mooocraft@users.noreply.github.com>
2018-10-28 10:03:14 -0400

    Add files via upload

---
+++ stanalone/sketch.js
@@ -0,0 +1,36 @@
+let editor;
+let turtle;
+
+function setup() {
+	createCanvas(200, 200);
+	angleMode(DEGREES);
+	background(0);
+	turtle = new Turtle(100, 100, 0);
+	editor = select('#code');
+	goTurtle();
+}
+
+function goTurtle() {
+	background(0);
+	push();
+	turtle.reset();
+	let code = editor.value();
+	let tokens = code.split(' ');
+	let index = 0;
+	while (index < tokens.length) {
+		let token = tokens[index];
+		if (commands[token]) {
+			if (token.charAt(0) === 'p' || token.charAt(0) === 'c') {
+				commands[token]();
+			}else if (token.includes("setxy")) {
+				console.log(index);
+				commands[token](tokens[++index], tokens[index+1]);
+				console.log(index);
+			}else {
+				commands[token](tokens[++index]);
+			}
+		}
+		index++;
+	}
+	pop();
+}
---
+++ stanalone/turtle.js
@@ -0,0 +1,65 @@
+const commands = {
+	"fd" : function (amt) {
+		turtle.forward(amt);
+	},
+	"bd" : function (amt) {
+		turtle.forward(-amt);
+	},
+	"rt" : function (angle) {
+		turtle.right(angle);
+	},
+	"lt" : function (angle) {
+		turtle.right(-angle);
+	},
+	"pu" : function () {
+		turtle.pen = false;
+	},
+	"pd" : function () {
+		turtle.pen = true;
+	},
+	"cs" : function () {
+		background(0);
+		turtle.right(90);
+	},
+	"setxy" : function (setX, setY) {
+		turtle.setxy(setX, setY);
+	},
+	"delay" : function (delay) {
+		var time = millis();
+		while(millis() - time <= delay);
+	}
+}
+
+class Turtle {
+	constructor(x, y, angle) {
+		this.x = x;
+		this.y = y;
+		this.dir = angle;
+	}
+
+	reset() {
+		translate(this.x, this.y);
+		rotate(this.dir);
+		this.pen = true;
+	}
+
+	forward(amt) {
+		amt = parseInt(amt);
+		if (this.pen) {
+			stroke(255);
+			strokeWeight(2);
+			console.log(amt);
+			line(0, 0, amt, 0);
+		}
+		translate(amt, 0);
+	}
+
+	right(angle) {
+		//console.log(angle);
+		rotate(angle);
+	}
+
+	setxy(setX, setY) {
+		translate(setX, setY);
+	}
+}
